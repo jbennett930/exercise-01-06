92185c4573d9f1faf4ff148f9855bf76042dfa7e
Jacson Bennett <jbennett930@west-mec.org>
2018-08-07 15:27:24 -0700

    Added code to help the make a logical algorithim to solve months with leap years.

--- snoot.js
+++ snoot.js
@@ -7,16 +7,54 @@
 */
 "use strict";
 
+var twentyNine = document.createDocumentFragment();
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
+
 // function to remove select list defaults
 function removeSelectDefaults() {
 var emptyBoxes = document.getElementsByTagName("select");
-alert("select lists:" + emptyBoxes.length);
+for (var i = 0; i < emptyBoxes.length; i++) {
+emptyBoxes[i].selectedIndex = -1;
+}
+}
+// Function to setup document fragments for days of the month.
+function setUpDays(){
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+
+}
+// function to set up the list of based on the selected month
+function updateDays() {
+    var deliveryDay = document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonth = deliveryMonth.options(deliveryMonth.selectedIndex).value;
+    while (dates[28]) {
+        deliveryDay.removeChild(dates[28]);
+    }
+    if (deliveryYear.selectedIndex === -1) {
+deliveryDay.selectedIndex = 0;
+    }
+}
+
+// Function that sets up page on load event
+function setUpPage() {
+removeSelectDefaults();
+setUpDays();
+updateDays();
 }
 
 // page load event handlers
 if (window.addEventListener) {
-    window.addEventListener("load", removeSelectDefaults, false);
+    window.addEventListener("load", setUpPage, false);
 }
 else if (window.attachEvent) {
-    window.attachEvent("onload", removeSelectDefaults);
+    window.attachEvent("onload", setUpPage);
 }
