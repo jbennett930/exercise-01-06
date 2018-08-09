cba7983bfcf662fca3f48434d9ffbafc6c4babfd
Unknown <jbennett930@west-mec.org>
2018-08-09 13:09:13 -0700

    Added functions to help create document fragments and put things directly into the DOM.

--- snoot.js
+++ snoot.js
@@ -35,21 +35,52 @@ function updateDays() {
     var dates = deliveryDay.getElementsByTagName("option");
     var deliveryMonth = document.getElementById("delivMo");
     var deliveryYear = document.getElementById("delivYr");
-    var selectedMonth = deliveryMonth.options(deliveryMonth.selectedIndex).value;
+    var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
     while (dates[28]) {
         deliveryDay.removeChild(dates[28]);
     }
     if (deliveryYear.selectedIndex === -1) {
 deliveryDay.selectedIndex = 0;
     }
+    // if its feb and 2020 twentyNine
+if (selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex].value === "2020") {
+deliveryDay.appendChild(twentyNine.cloneNode(true));
 }
+    // else if 30 day month thirty
+else if (selectedMonth === "4" || selectedMonth === "6" || selectedMonth === "9" || selectedMonth === "11") {
+    deliveryDay.appendChild(thirty.cloneNode(true));
+}
+    // else if 31 days thirtyOne
+    else if (selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth === "7" || selectedMonth === "12") {
+        deliveryDay.appendChild(thirtyOne.cloneNode(true));
+    }
+}
+
 
 // Function that sets up page on load event
 function setUpPage() {
 removeSelectDefaults();
 setUpDays();
-updateDays();
+createEventListeners();
 }
+
+// function to create our event listeners
+function createEventListeners() {
+    var deliveryMonth = document.getElementById("delivMo");
+    if (deliveryMonth.addEventListener) {
+        deliveryMonth.addEventListener("change", updateDays, false);
+    }
+    else if (deliveryMonth.attachEvent) {
+        deliveryMonth.attachEvent("onchange", updateDays);
+    }
+    var deliveryYear = document.getElementById("delivYr");
+    if (deliveryYear.addEventListener) {
+        deliveryYear.addEventListener("change", updateDays, false);
+    }
+    else if (deliveryYear.attachEvent) {
+        deliveryYear.attachEvent("onchange", updateDays);
+    }
+    }
 
 // page load event handlers
 if (window.addEventListener) {
